a513ec100a883f9da3fe7f9cb8376ecf45bf5c73
Ivan Costa <ivan.felipe@payshopx.com.br>
2021-03-24 22:00:19 -0300

    feat: sharing dependencies between apps;
    Signed-off-by: Ivan Costa <ivan.felipe@payshopx.com.br>

--- ecomm/container/src/bootstrap.js
+++ ecomm/container/src/bootstrap.js
@@ -1,2 +1,5 @@
-import "products/ProductsIndex";
-import "cart/CartShow";
+import { mount as cartMount } from "cart/CartShow";
+import { mount as productMount } from "products/ProductsIndex";
+
+productMount(document.querySelector("#dev-products"));
+cartMount(document.querySelector("#dev-cart"));
