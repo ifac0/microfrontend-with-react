04383a42400cbc42e285f42d22a96c095fc6ab49
Ivan Costa <ivan.felipe@payshopx.com.br>
2021-03-26 19:14:30 -0300

    feat: generic ties between projects;
    Signed-off-by: Ivan Costa <ivan.felipe@payshopx.com.br>

---
+++ mfp/packages/container/src/App.js
@@ -0,0 +1,12 @@
+import MarketingApp from "./components/MarketingApp";
+import React from "react";
+
+export default () => {
+  return (
+    <div>
+      <h1>Hi there!</h1>
+      <hr />
+      <MarketingApp />
+    </div>
+  );
+};
